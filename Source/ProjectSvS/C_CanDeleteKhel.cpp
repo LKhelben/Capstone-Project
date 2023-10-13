b5faec68a734f5208f47ad9681dc6fa74439e76f
LKhelben <lkhelben@yahoo.com>
2023-10-13 06:39:23 -0400

    - Fixed Corrupted Structure in Profile_Info - Added a C++ class for recompiling

---
+++ Source/ProjectSvS/C_CanDeleteKhel.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_CanDeleteKhel.h"
+
+// Sets default values
+AC_CanDeleteKhel::AC_CanDeleteKhel()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AC_CanDeleteKhel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AC_CanDeleteKhel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AC_CanDeleteKhel::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
